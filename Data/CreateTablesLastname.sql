0764ba898b25690f85f7bb893905851f2ea5ef21
Sabrina Harsh <srh00041@mix.wvu.edu>
2026-02-17 12:28:55 -0500

    Updated Conference Division table

--- Data/CreateTablesLastname.sql
+++ Data/CreateTablesLastname.sql
@@ -1,1 +1,42 @@
+--create database for NFL app
+--use master;
+--CREATE DATABASE MIST353_NFL_RDB_Harsh;
+--DROP database NFL_RDB_Harsh;
+
 --create tables for first iteration
+use MIST353_NFL_RDB_Harsh;
+
+if(OBJECT_ID('Team') is not null)
+    drop table Team;
+if(OBJECT_ID('ConferenceDivision') is not null)
+    drop table ConferenceDivision;
+
+
+go
+create TABLE ConferenceDivision (
+    ConferenceDivisionID INT identity(1,1)
+        constraint PK_ConfeenceDivision PRIMARY KEY,
+    Conference NVARCHAR(50) NOT NULL
+        constraint CK_ConferenceNames CHECK (Conference IN ('AFC', 'NFC')),
+    Division NVARCHAR(50) NOT NULL
+        constraint CK_DivisionNames CHECK (Division IN ('East', 'North', 'South', 'West')),
+    constraint UK_ConferenceDiVision UNIQUE (Conference, Division)
+);
+/*
+alter table ConferenceDivision
+    NOCHECK CONSTRAINT CK_ConferenceNames;
+
+alter table ConferenceDivision
+    CHECK CONSTRAINT CK_DivisionNames;
+    */
+
+go
+create TABLE Team (
+    TeamID INT identity(1,1)
+        constraint PK_Twam PRIMARY KEY,
+    TeamName NVARCHAR(50) NOT NULL,
+    TeamColors NVARCHAR(50) NOT NULL,
+    ConferenceDivisionID INT NOT NULL  
+        constraint FK_Team_ConferenceDivision FOREIGN KEY REFERENCES ConferenceDivision(ConferenceDivisionID)
+);
+
