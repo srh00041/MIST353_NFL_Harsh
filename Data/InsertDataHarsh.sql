ae6df3f26145b2c9bb55b64b47026e74ff35b5d3
Sabrina Harsh <srh00041@mix.wvu.edu>
2026-02-18 16:20:53 -0500

    wrote 3 queries

--- Data/InsertDataHarsh.sql
+++ Data/InsertDataHarsh.sql
@@ -1,7 +1,8 @@
 -- insert data
 --insert all the ConferenceDivision data (8 rows)
+use MIST353_NFL_RDB_Harsh;
 
-/*insert into ConferenceDivision (Conference, Division)
+insert into ConferenceDivision (Conference, Division)
 values ('AFC', 'North'),
         ('AFC', 'South'), 
         ('AFC', 'East'), 
@@ -12,19 +13,20 @@ values ('AFC', 'North'),
         ('NFC', 'West');
 
 GO
-select * from ConferenceDivision;
+select * from ConferenceDivision order by ConferenceDivisionID;
+
 
 GO
 insert into Team (TeamName, TeamCityState, TeamColors, ConferenceDivisionID)
-values ('Balitmore Ravens', 'Baltimore, MD', 'Purple, Black, Metallic Gold', 1),
+values ('Baltimore Ravens', 'Baltimore, MD', 'Purple, Black, Metallic Gold', 1),
         ('Cincinnati Bengals', 'Cincinnati, OH', 'Black, Orange, White', 1),
-        ('Cleveland Browns', 'Cleveland, OH', 'Brown, Organge, White', 1),
+        ('Cleveland Browns', 'Cleveland, OH', 'Brown, Orange, White', 1),
         ('Pittsburgh Steelers', 'Pittsburgh, PA', 'Black, Gold, White', 1),
 
         ('Houston Texans', 'Houston, TX', 'Deep Steel Blue, Battle Red, Liberty White', 2),
         ('Indianapolis Colts', 'Indianapolis, IN', 'Speed Blue, White', 2),
         ('Jacksonville Jaguars', 'Jacksonville, FL', 'Teal, Black, Gold, White', 2),
-        ('Tennesse Titans', 'Nashville, TN', 'Titans Navy, Titans Blue, Red, Silver', 2),
+        ('Tennessee Titans', 'Nashville, TN', 'Titans Navy, Titans Blue, Red, Silver', 2),
 
         ('Buffalo Bills', 'Buffalo, NY', 'Royal Blue, Red, White', 3),
         ('Miami Dolphins', 'Miami, FL', 'Aqua Green, Orange, White', 3),
@@ -32,9 +34,9 @@ values ('Balitmore Ravens', 'Baltimore, MD', 'Purple, Black, Metallic Gold', 1),
         ('New York Jets', 'East Rutherford, NJ', 'Gotham Green, Spotlight White, Stealth Black', 3),
 
         ('Denver Broncos', 'Denver, CO', 'Broncos Orange, Navy Blue, White', 4),
-        ('Kansas City Chiefs', 'Kansas City, MO', 'Red, Goldm White', 4),
+        ('Kansas City Chiefs', 'Kansas City, MO', 'Red, Gold, White', 4),
         ('Las Vegas Raiders', 'Las Vegas, NV', 'Silver and Black', 4),
-        ('Los Angeles Chargers', 'Los Angeles, CA', 'Powder BLue, Sunshine Gold, White', 4),
+        ('Los Angeles Chargers', 'Los Angeles, CA', 'Powder Blue, Sunshine Gold, White', 4),
 
         ('Chicago Bears', 'Chicago, IL', 'Navy Blue, Orange, White', 5),
         ('Detroit Lions', 'Detroit, MI', 'Honolulu Blue, Silver, White', 5),
@@ -49,12 +51,14 @@ values ('Balitmore Ravens', 'Baltimore, MD', 'Purple, Black, Metallic Gold', 1),
         ('Dallas Cowboys', 'Dallas, TX', 'Navy Blue, Metallic Silver, White', 7),
         ('New York Giants', 'East Rutherford, NJ', 'Royal Blue, Red, White', 7),
         ('Philadelphia Eagles', 'Philadelphia, PA', 'Midnight Green, Silver Metallic, Black Metallic, White', 7),
-        ('Washington Commanders', 'Washington D.C.', 'Burgandy, Gold', 7),
+        ('Washington Commanders', 'Washington D.C.', 'Burgundy, Gold', 7),
 
-        ('Arizona Cardinals', 'Pheonix, AZ', 'Cardinal Red, Black, White', 8),
-        ('Los Angeles Rams', 'Los Angeles, CA', 'Royal BLue, Gold', 8),
+        ('Arizona Cardinals', 'Phoenix, AZ', 'Cardinal Red, Black, White', 8),
+        ('Los Angeles Rams', 'Los Angeles, CA', 'Royal Blue, Gold', 8),
         ('San Francisco 49ers', 'San Francisco, CA', 'Scarlet, Gold', 8),
-        ('Seattle Seahawks', 'Seattle, WA', 'College Navy, Action Green', 8);*/
+        ('Seattle Seahawks', 'Seattle, WA', 'College Navy, Action Green', 8);
+
+select * from Team;
 
 
         
