44a3c74c7d3a5508fb4ada5dbced1d1de7667b88
Sabrina Harsh <srh00041@mix.wvu.edu>
2026-04-20 22:11:33 -0400

    changes

--- UI/get_teams_for_specified_fan_ui.py
+++ UI/get_teams_for_specified_fan_ui.py
@@ -4,7 +4,8 @@ from fetch_data import fetch_data
 def get_teams_for_specified_fan_ui():
     st.header("Get Teams for Specified Fan")
 
-    nfl_fan_id = st.number_input("Enter Fan ID", min_value=1, step=1)
+    #nfl_fan_id = st.number_input("Enter Fan ID", min_value=1, step=1)
+    nfl_fan_id = st.text_input("Enter Fan ID", value = st.session_state.app_user_id, disabled=True)
     if st.button("Fetch Teams"):
         if not nfl_fan_id:
             st.warning("Please Enter a NFL Fan ID")
@@ -15,7 +16,7 @@ def get_teams_for_specified_fan_ui():
             df = fetch_data("get_teams_for_specified_fan/", input_params)
 
             if df is not None and not df.empty:
-                st.subheader(f"teams for NFL Fan ID {nfl_fan_id}:")
+                st.success(f"teams for NFL Fan ID {nfl_fan_id}:")
                 st.dataframe(df, use_container_width=True, hide_index=True)
             else:
                 st.info(f"No teams found for NFL Fan ID {nfl_fan_id}. Please check the fan ID and try again.")
